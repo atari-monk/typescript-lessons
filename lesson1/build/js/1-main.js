4b06914f070e3abdcbba18403dd4872907be8fcd
atari-monk <atari.monk1@gmail.com>
2023-02-21 17:01:47 +0100

    add lessons

---
+++ lesson1/build/js/1-main.js
@@ -0,0 +1,15 @@
+"use strict";
+let myName = "Dave";
+let age;
+let isLoading;
+let album;
+myName = "Jhon";
+age = 42;
+isLoading = true;
+album = true;
+const add = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
---
+++ lesson1/src/1-main.ts
@@ -0,0 +1,18 @@
+let myName: string = "Dave";
+let age: number;
+let isLoading: boolean;
+let album: any;
+
+myName = "Jhon";
+age = 42;
+isLoading = true;
+album = true;
+
+const add = (a: number, b: number) => {
+  return a + b;
+};
+
+let postId: string | number;
+let isActive: boolean | number | string;
+
+let re: RegExp = /\w+/g;
